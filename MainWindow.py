645b4d4ee6e05043c6a102fb4af8b8ae5cc73004
HTMLlama <andrew@hugheswd.com>
2019-12-08 10:34:25 -0800

    Added controls and gifs

--- MainWindow.py
+++ MainWindow.py
@@ -1,10 +1,17 @@
-from pyglet.gl import *
+import pyglet, sys, os
+
+from Window import Window
 
 
-class MainWindow(pyglet.window.Window):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.set_maximum_size(600, 480)
+# def reexec_with_pythonw():
+#     if sys.platform == 'darwin' and\
+#            not sys.executable.endswith('MacOS/Python'):
+#         print >>sys.stderr,'re-executing using pythonw'
+#         os.execvp('pythonw',['pythonw',__file__] + sys.argv[1:])
+#
 
-    def on_draw(self):
-        self.clear()
+if __name__ == '__main__':
+    window = Window()
+    pyglet.clock.schedule_interval(window.update, 1 // 60)
+    pyglet.app.run()
+
